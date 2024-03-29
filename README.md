333abbd6d7d1d213803568a16a59c1162b8f115a
Xinjian Zhang <117434829+Xinjian-Zhang@users.noreply.github.com>
2024-03-29 20:17:05 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-<h1 align="center">喜马拉雅xm文件解密工具</h1>
-<h4 align="center">Ximalaya-XM-Decrypt</h4>
+# 喜马拉\* xm文件解密工具 简易GUI版
 
 ### 说明
 
-由于喜马拉雅官方客户端下载的音频文件为加密格式，无法在普通播放器中播放，所以我写了这个小工具来解密喜马拉雅下载的音频文件。本工具参考@aynakeya的[博文](https://www.aynakeya.com/2023/03/15/ctf/xi-ma-la-ya-xm-wen-jian-jie-mi-ni-xiang-fen-xi/)，并加入了批量解密的功能。我还写了一个程序[Ximalaya-Downloader](https://github.com/Diaoxiaozhang/Ximalaya-Downloader)，用于直接爬取未加密的喜马拉雅音频文件。本工具作为Ximalaya-Downloader的补充，当每日下载vip音频达到上限时，可以使用客户端下载加密的xm文件并使用本工具解密。
+本人书友，有离线下载音频需求。看到原作者的代码后，简单地套了一个GUI，方便使用
+
+打开后可选择单个文件或文件夹，然后选择输出目录，若取消选择输出目录，则默认输出到根目录下，自动开始转换，完成后弹框提示。
+
+
+对于Windows用户，可以直接下载简单封装的.exe [Release]()
+
+**感谢**<br>
+原作者：[Daxiaozhang](https://github.com/Diaoxiaozhang/)
+原项目：[Repo](https://github.com/Diaoxiaozhang/Ximalaya-XM-Decrypt)
+
+----
+----
+
+### 以下为原作者README
+
+<h1 align="center">喜马拉*xm文件解密工具</h1>
+<h4 align="center">Ximalaya-XM-Decrypt</h4>
+
+
+### 说明
+
+由于喜马拉\*官方客户端下载的音频文件为加密格式，无法在普通播放器中播放，所以我写了这个小工具来解密喜马拉\*下载的音频文件。本工具参考@aynakeya的[博文](https://www.aynakeya.com/2023/03/15/ctf/xi-ma-la-ya-xm-wen-jian-jie-mi-ni-xiang-fen-xi/)，并加入了批量解密的功能。我还写了一个程序[Ximalaya-Downloader](https://github.com/Diaoxiaozhang/Ximalaya-Downloader)，用于直接爬取未加密的喜马拉\*音频文件。本工具作为Ximalaya-Downloader的补充，当每日下载vip音频达到上限时，可以使用客户端下载加密的xm文件并使用本工具解密。
 
 在使用该软件时，请确保xm_encryptor.wasm文件与主程序文件处在同一目录下，最好是一个单独的文件夹。
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import base64
 import io
-import sys
 import magic
 import pathlib
 import os
 import glob
 import mutagen
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, messagebox
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad
 from mutagen.easyid3 import ID3
 from wasmer import Store, Module, Instance, Uint8Array, Int32Array, engine
 from wasmer_compiler_cranelift import Compiler
+import webbrowser
 
 
 class XMInfo:
@@ -43,10 +43,7 @@ def read_file(x):
     with open(x, "rb") as f:
         return f.read()
 
-
-# return number of id3 bytes
 def get_xm_info(data: bytes):
-    # print(EasyID3(io.BytesIO(data)))
     id3 = ID3(io.BytesIO(data), v2_version=3)
     id3value = XMInfo()
     id3value.title = str(id3["TIT2"])
@@ -62,9 +59,7 @@ def get_xm_info(data: bytes):
 
 
 def get_printable_count(x: bytes):
-    i = 0
     for i, c in enumerate(x):
-        # all pritable
         if c < 0x20 or c > 0x7e:
             return i
     return i
@@ -75,35 +70,22 @@ def get_printable_bytes(x: bytes):
 
 
 def xm_decrypt(raw_data):
-    # load xm encryptor
-    # print("loading xm encryptor")
     xm_encryptor = Instance(Module(
         Store(engine.Universal(Compiler)),
         pathlib.Path("./xm_encryptor.wasm").read_bytes()
     ))
-    # decode id3
     xm_info = get_xm_info(raw_data)
-    # print("id3 header size: ", hex(xm_info.header_size))
     encrypted_data = raw_data[xm_info.header_size:xm_info.header_size + xm_info.size:]
 
-    # Stage 1 aes-256-cbc
     xm_key = b"ximalayaximalayaximalayaximalaya"
-    # print(f"decrypt stage 1 (aes-256-cbc):\n"
-    #       f"    data length = {len(encrypted_data)},\n"
-    #       f"    key = {xm_key},\n"
-    #       f"    iv = {xm_info.iv().hex()}")
     cipher = AES.new(xm_key, AES.MODE_CBC, xm_info.iv())
     de_data = cipher.decrypt(pad(encrypted_data, 16))
-    # print("success")
-    # Stage 2 xmDecrypt
+
     de_data = get_printable_bytes(de_data)
     track_id = str(xm_info.tracknumber).encode()
     stack_pointer = xm_encryptor.exports.a(-16)
-    assert isinstance(stack_pointer, int)
     de_data_offset = xm_encryptor.exports.c(len(de_data))
-    assert isinstance(de_data_offset, int)
     track_id_offset = xm_encryptor.exports.c(len(track_id))
-    assert isinstance(track_id_offset, int)
     memory_i = xm_encryptor.exports.i
     memview_unit8: Uint8Array = memory_i.uint8_view(offset=de_data_offset)
     for i, b in enumerate(de_data):
@@ -111,23 +93,16 @@ def xm_decrypt(raw_data):
     memview_unit8: Uint8Array = memory_i.uint8_view(offset=track_id_offset)
     for i, b in enumerate(track_id):
         memview_unit8[i] = b
-    # print(bytearray(memory_i.buffer)[track_id_offset:track_id_offset + len(track_id)].decode())
-    # print(f"decrypt stage 2 (xmDecrypt):\n"
-    #       f"    stack_pointer = {stack_pointer},\n"
-    #       f"    data_pointer = {de_data_offset}, data_length = {len(de_data)},\n"
-    #       f"    track_id_pointer = {track_id_offset}, track_id_length = {len(track_id)}")
-    # print("success")
+
     xm_encryptor.exports.g(stack_pointer, de_data_offset, len(de_data), track_id_offset, len(track_id))
     memview_int32: Int32Array = memory_i.int32_view(offset=stack_pointer // 4)
     result_pointer = memview_int32[0]
     result_length = memview_int32[1]
     assert memview_int32[2] == 0, memview_int32[3] == 0
     result_data = bytearray(memory_i.buffer)[result_pointer:result_pointer + result_length].decode()
-    # Stage 3 combine
-    # print(f"Stage 3 (base64)")
+
     decrypted_data = base64.b64decode(xm_info.encoding_technology + result_data)
     final_data = decrypted_data + raw_data[xm_info.header_size + xm_info.size::]
-    # print("success")
     return xm_info, final_data
 
 
@@ -141,7 +116,6 @@ def find_ext(data):
 
 
 def decrypt_xm_file(from_file, output_path='./output'):
-    print(f"正在解密{from_file}")
     data = read_file(from_file)
     info, audio_data = xm_decrypt(data)
     output = f"{output_path}/{replace_invalid_chars(info.album)}/{replace_invalid_chars(info.title)}.{find_ext(audio_data[:0xff])}"
@@ -152,12 +126,11 @@ def decrypt_xm_file(from_file, output_path='./output'):
     tags["title"] = info.title
     tags["album"] = info.album
     tags["artist"] = info.artist
-    print(tags.pprint())
     tags.save(buffer)
     with open(output, "wb") as f:
         buffer.seek(0)
         f.write(buffer.read())
-    print(f"解密成功，文件保存至{output}！")
+    return output
 
 
 def replace_invalid_chars(name):
@@ -184,41 +157,67 @@ def select_directory():
     return directory_path
 
 
+def single_file_decrypt():
+    file_to_decrypt = select_file()
+    output_dir_path = output_dir()
+    if file_to_decrypt:
+        decrypted_file = decrypt_xm_file(file_to_decrypt, output_dir_path)
+        tk.messagebox.showinfo("转换完成", f"已将文件转为在 {decrypted_file}")
+    else:
+        print("未选择文件")
+
+
+def batch_decrypt():
+    dir_to_decrypt = select_directory()
+    output_dir_path = output_dir()  # Get the output directory path
+    if dir_to_decrypt:
+        files_to_decrypt = glob.glob(os.path.join(dir_to_decrypt, "*.xm"))
+        for file in files_to_decrypt:
+            decrypted_file = decrypt_xm_file(file, output_dir_path)
+
+        tk.messagebox.showinfo("转换完成", f"已将文件转为在 {output_dir_path}")
+    else:
+        print("未选择目录")
+
+def output_dir():
+    root = tk.Tk()
+    root.withdraw()
+    directory_path = filedialog.askdirectory(title="选择输出文件夹")
+    root.destroy()
+    return directory_path
+
+def show_author_info():
+    messagebox.showinfo("关于作者", "GUI窗体: Xinjian-Zhang\n 解密算法及源码: Diaoxiaozhang")
+
+def open_github():
+    webbrowser.open("https://github.com/Xinjian-Zhang/Ximalaya-XM-Decrypt-GUI")
+
+def main():
+    root = tk.Tk()
+    root.title("喜马拉*音频解密工具")
+    root.geometry("300x180")
+
+    single_file_button = tk.Button(root, text="解密单个文件", command=single_file_decrypt)
+    single_file_button.pack(pady=10)
+
+    batch_button = tk.Button(root, text="批量解密文件", command=batch_decrypt)
+    batch_button.pack(pady=10)
+
+
+    exit_button = tk.Button(root, text="退出", command=root.quit)
+    exit_button.pack(pady=10)
+
+    menubar = tk.Menu(root)
+    about_menu = tk.Menu(menubar, tearoff=0)
+
+    about_menu.add_command(label="作者信息", command=show_author_info)
+    about_menu.add_separator()
+    about_menu.add_command(label="GitHub", command=open_github)
+    root.config(menu=menubar)
+    menubar.add_cascade(label="关于", menu=about_menu)
+
+    root.mainloop()
+
+
 if __name__ == "__main__":
-    while True:
-        print("欢迎使用喜马拉雅音频解密工具")
-        print("本工具仅供学习交流使用，严禁用于商业用途")
-        print("请选择您想要使用的功能：")
-        print("1. 解密单个文件")
-        print("2. 批量解密文件")
-        print("3. 退出")
-        choice = input()
-        if choice == "1" or choice == "2":
-            if choice == "1":
-                files_to_decrypt = [select_file()]
-                if files_to_decrypt == [""]:
-                    print("检测到文件选择窗口被关闭")
-                    continue
-            elif choice == "2":
-                dir_to_decrypt = select_directory()
-                if dir_to_decrypt == "":
-                    print("检测到目录选择窗口被关闭")
-                    continue
-                files_to_decrypt = glob.glob(os.path.join(dir_to_decrypt, "*.xm"))
-            print("请选择是否需要设置输出路径：（不设置默认为本程序目录下的output文件夹）")
-            print("1. 设置输出路径")
-            print("2. 不设置输出路径")
-            choice = input()
-            if choice == "1":
-                output_path = select_directory()
-                if output_path == "":
-                    print("检测到目录选择窗口被关闭")
-                    continue
-            elif choice == "2":
-                output_path = ""
-            for file in files_to_decrypt:
-                decrypt_xm_file(file, output_path)
-        elif choice == "3":
-            sys.exit()
-        else:
-            print("输入错误，请重新输入！")
+    main()
